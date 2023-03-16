73727c54750ac3fb6848218f469a28acc8480fe3
EssenceCode <123456789+odin@users.noreply.github.com>
2023-03-16 16:32:39 +0800

    javascript: move the score increment

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# rock-paper-scissors
+# rock-paper-scissors 
+
+
+
+# Pseudo Code
+1. create an function that will randomly return either rock, paper or scissors.
+
+2. create a function that will play once with two parameters 
+ a. ask the player for an input
+ b. generate a random play for the computer
+ c. compare the player's input to the computer's input to determine who won the round
+ d. display the result of each round to the user   
+
+3. set a variables declaring who won to call
+
+4. set the players and computer scores to 0 
+
+5. create a function that will play 5 rounds.
+   a. set a loop for 5 rounds and call the function that will play inside
+   b. compare the player's play to the computer's play and determine a winner
+   c. update the score based on who won or if it was tie
+   d. determine who won based on their scores
+
+6. display the final score and declare a winner   
--- javascript.js
+++ javascript.js
@@ -1,92 +1,69 @@
-// a function that randomly return either rock,paper or scissors
-
-    function getComputerChoice() {
-      let computerRandom = Math.floor(Math.random() * 3 ) + 1;
-     
-      if(computerRandom === 1) {
-          return "rock"; 
-    }
-     
-    else if(computerRandom === 2) {
-        return "paper";
-    }
-    
-    else if(computerRandom === 3) {
-        return "scissors";
-    }                
-}
 
     //Write a function that plays a single round of Rock Paper Scissors.
-
-
-    function playRound(player,computer){
- 
-    player = prompt('choose rock, paper or scissors').toLowerCase();
-    computer = getComputerChoice();
+    function playRound(player,computer) {
+        const choice = ["rock", "paper", "scissors"];
+        let choiceRandom = choice[Math.floor(Math.random()*choice.length)];
+        player = prompt("Choice").toLowerCase();
+        computer = choiceRandom;
+        console.log("PlayerChoice:" + player);
+        console.log("ComputerChoice:" + computer);
    
-     if (player === "rock" && computer === "scissors" || player === "paper" && computer === "rock" ||player === "scissors" && computer === "paper" ) {
-          return win; // return statement can return a variable within a scope of a function(){}
+       if (player === "rock" && computer === "scissors" || player === "paper" && computer === "rock" || player === "scissors" && computer === "paper") {
+           playerScore++;      
+           return win; 
+         }   
+       
+       else if  (computer === "rock" && player === "scissors" || computer === "paper" && player === "rock" || computer === "scissors" && player === "paper") {
+                 computerScore++;
+                 return lose;
+          }
+   
+       else if (player === computer) {
+                drawScore++; 
+                return draw;
+          }   
      }
-     
-     else if (computer === "rock" && player === "scissors" || computer === "paper" && player === "rock" ||computer === "scissors" && player === "paper" )  {
-        return lose;
-    } 
     
-    else if (player === computer) { 
-        return draw;
-    }
-}
- //define win , lose or draw (global variable}         
-    const win = "Player won the round"; 
-    const lose = "Computer won the round";
-    const draw = "Draw";
-
-// data score(global variable)
-    let playScore = 0;
-    let compScore = 0;
-    let drawScore = 0; 
-//let roundScore = playScore+ compScore+drawScore; by placing it here it will not capture the changes because let is use a keyword used to declare block scoped variable
-// meaning the score variable will not change until the function game(){} is called
-
-// function that will play 5 times with aid of function playRound(){...}
-    function playGame() { 
-// create a loop
-    for (let i = 0; i < 5; i++) { 
-      let result = playRound();
+       
+    //define win , lose or draw (global variable}         
+       const win = "Outcome:Very nice! You won"; 
+       const lose = "Outcome:Nice try! You lose";
+       const draw = "Outcome:Draw";
+   
+   
+   // score store(global variable)
+      let playerScore = 0;
+      let computerScore = 0;
+      let drawScore = 0;
+   
+    // this will be updated,because iterator is a global variable
+    
+   // a function that will play Rock, Paper and Scissors 5 times with aid of playRound(...){...}
+     function playGame() {
+     // create a loop
+     for ( let i = 0; i < 5; ++i) {
+        let result = playRound(); 
         console.log(result);
+   
+       }
+         // score comparison to determine who won the game or draw
+         if (playerScore > computerScore) { // an if statement that determines who won (or draw) the game based on the score variables
+              return "Player wins"; 
+            }
+         else if (playerScore  === computerScore) {
+               return "Nobody wins";
+            } else {
+               return "Computer wins";
+            }
+         
+       
+     
+   }
     
-    // increasing the score variable based on who won the round    
-    if (result === win) { 
-        playScore++;
-    }   
-
-    else if (result === lose) {
-            compScore++;
-    } 
-
-    else if (result === draw) {
-            drawScore++;}
-    }
-    
-    // if statement that determines who won the game based on score variables
-    if (playScore > compScore) { 
-           return "Player wins"; 
-    }
-    
-    else if (playScore  === compScore) {
-            return "Nobody wins";
-    }else {
-        return "Computer wins";
-    }
-}
-//console.log(playGame());//to play the game
-    console.log("Your final score is " + playScore + " " + "wins" + " " + compScore + " " + "loses" + " " + drawScore +" " + "draw");// declaring the scores
-// final score
-    let roundScore = playScore+ compScore+drawScore;
-// declaring how many rounds
-    console.log("Total rounds" + " " + roundScore + " " + "times.");
-
-
-
-
-    
+     
+   console.log(playGame());
+   console.log("Your final score is " + playerScore + " " + "wins" + " " + computerScore + " " + "loses" + " " + drawScore +" " + "draw");// declaring the scores
+   //Declare the total rounds
+   let roundTotal = playerScore+ computerScore + drawScore;
+   console.log("Total rounds" + " " + roundTotal + " " + "times.");// declaring how many rounds
+   
