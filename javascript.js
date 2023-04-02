392312c823283f0f5bb2fc3e0b02f2e755699335
EssenceCode <123456789+odin@users.noreply.github.com>
2023-04-02 13:10:26 +0800

    added a score for each play

--- javascript.js
+++ javascript.js
@@ -13,19 +13,36 @@ function computerChoice () {
 // create a function that will play 1 round of rps
 
   function playRound (player, computer) {
- 
+    
+  
+   if  (playerScore === 5 ) {
+       document.getElementById("resultsF").textContent = 'Round:Player wins ';  
+    }   
+   else if (computerScore === 5) {
+       document.getElementById("resultsF").textContent = 'Round:Computer wins '; 
+    }
+   else if (playerScore === 5 || computerScore === 5 ) {
+       document.getElementById("resultsF").textContent = 'Round:Draw '; 
+    }     
+
    if (player === "rock" && computer === "scissors" || player === "paper" && computer === "rock" || player === "scissors" && computer === "paper") {
-       return "win"; 
+      playerScore ++;
+      document.getElementById("results").textContent = 'you win ' + playerScore;      
+       
     }
    else if (computer === "rock" && player === "scissors" || computer === "paper" && player === "rock" || computer === "scissors" && player === "paper") {
-       return "lose"; 
+       computerScore ++;
+       document.getElementById("results").textContent = 'you lose '  + computerScore;       
     }
    else if (player === computer) {
-       return "draw"; 
+       
+       document.getElementById("results").textContent = 'it\'s draw ';        
     } 
 
 }
  
+// parent
+const buttons = document.querySelector('#buttons');
 
 // select the child nodes
   const rock = document.querySelector(".rock");
@@ -33,18 +50,29 @@ function computerChoice () {
   const scissors = document.querySelector(".scissors"); 
 // add addeventlistener to the child nodes
   rock.addEventListener('click', () => {
-     console.log(playRound("rock", computerChoice()));  
-   
+     playRound("rock", computerChoice());  
+   //console.log(playRound("rock", computerChoice()));   
  })  
 
- paper.addEventListener('click', () => {
-      
- playRound("paper", computerChoice());  
-    console.log(playRound("paper", computerChoice()));  
+ paper.addEventListener('click', () => {     
+     playRound("paper", computerChoice());  
+    //console.log(playRound("paper", computerChoice()));  
  })  
 
  scissors.addEventListener('click', () => {
-     console.log(playRound("scissors", computerChoice()));  
+     playRound("scissors", computerChoice());  
+   //console.log(playRound("scissors", computerChoice()));  
        
  });  
-    
+
+// create a div to store results
+  const results = document.createElement('div');
+  results.setAttribute('id','results');
+  buttons.appendChild(results);  
+  const resultsF = document.createElement('div');
+  resultsF.setAttribute('id','resultsF')
+  buttons.appendChild(resultsF); 
+// variable to store scores
+  let playerScore = 0;
+  let computerScore = 0;
+//let drawScore = 0; don't need to count draw scores   
